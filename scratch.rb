9f98fa6beb45a3f2d30928fcd75be8c9e7e26008
Stan Carver II <stan@a1webconsulting.com>
2026-04-24 11:40:44 -0500

    removed temporary files

--- scratch.rb
+++
@@ -1,4 +0,0 @@
-require 'pathname'
-path = Pathname.new('/Users/scarver2/Library/Application Support/AddressBook')
-puts "Root dbs: #{path.glob('*.abcddb').map(&:to_s)}"
-puts "All dbs: #{path.glob('**/*.abcddb').map(&:to_s)}"
